f8ef4b16f6c89ccdb61ca87d3355626b0bdc9194
jesseegedegbe3-collab <jesseegedegbe3@gmail.com>
2026-08-18 15:38:48 -0500

    Set base path for deployment in Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/best-painting-redesign/',
   plugins: [vlyPlugin(), react(), tailwindcss()],
   resolve: {
     alias: {
